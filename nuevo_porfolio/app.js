635ff85273f279d06870ca85729df3afcd73da13
francobosg <francobosg@gmail.com>
2025-04-23 11:33:14 +0200

    cookies

--- nuevo_porfolio/app.js
+++ nuevo_porfolio/app.js
@@ -1,5 +1,5 @@
-// Video hover effects
-const videoList = [
+ // Video hover effects
+ const videoList = [
     document.getElementById('projectVideo1'),
     document.getElementById('projectVideo2'),
     document.getElementById('projectVideo3')
@@ -23,4 +23,41 @@ const videoList = [
     const progress = bar.dataset.progress;
     bar.style.setProperty('--hover-progress', `${progress}%`);
   });
-  
+
+  // Cookie banner functionality
+  const cookieBanner = document.querySelector('.cookie-banner');
+  const acceptBtn = document.querySelector('#accept-cookies');
+  const rejectBtn = document.querySelector('#reject-cookies');
+  const readMoreBtn = document.querySelector('#read-more');
+
+  function hideCookieBanner() {
+    localStorage.setItem('cookieConsent', 'true');
+    cookieBanner.style.display = 'none';
+    document.body.style.overflow = 'auto'; // Habilita el desplazamiento de la página
+    document.body.classList.remove('no-scroll'); // Elimina la clase que bloquea el scroll
+    document.body.style.position = ''; // Elimina el estilo de posición que bloquea el scroll
+    document.body.style.paddingRight = ''; // Restablece el padding en el body
+    document.body.style.setProperty('backdrop-filter', 'none'); // Elimina el fondo difuminado
+  }
+
+  function rejectCookies() {
+    localStorage.setItem('cookieConsent', 'false');
+    cookieBanner.style.display = 'none';
+    document.body.style.overflow = 'auto'; // Habilita el desplazamiento de la página
+    document.body.classList.remove('no-scroll'); // Elimina la clase que bloquea el scroll
+    document.body.style.position = ''; // Elimina el estilo de posición que bloquea el scroll
+    document.body.style.paddingRight = ''; // Restablece el padding en el body
+    document.body.style.setProperty('backdrop-filter', 'none'); // Elimina el fondo difuminado
+  }
+
+  acceptBtn.addEventListener('click', hideCookieBanner);
+  rejectBtn.addEventListener('click', rejectCookies);
+
+  if (localStorage.getItem('cookieConsent') === 'true') {
+    cookieBanner.style.display = 'none';
+    document.body.style.overflow = 'auto'; // Habilita el desplazamiento
+    document.body.classList.remove('no-scroll'); // Elimina la clase de no-scroll
+    document.body.style.position = ''; // Elimina el estilo de posición
+    document.body.style.paddingRight = ''; // Restablece el padding
+    document.body.style.setProperty('backdrop-filter', 'none'); // Elimina el fondo difuminado
+  }
