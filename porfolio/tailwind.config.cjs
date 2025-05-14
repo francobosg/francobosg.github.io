dd1137b017987e4667a2839449fac27ef706b732
francobosg <francobosg@gmail.com>
2025-05-14 15:03:32 +0200

    f

--- porfolio/tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./assets/js/*.js", "./*.{html,js}"],
-  darkMode: 'class',
-  theme: {
-    extend: {},
-  },
-  plugins: [require('flowbite/plugin')],
-}
